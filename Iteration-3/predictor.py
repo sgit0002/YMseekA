55ebc1f019b693f02ae5783d6008337daf656fcc
Ram <sgit0002@student.monash.edu>
2021-09-16 02:33:17 +1000

    adding predictor files

--- Iteration-3/predictor.py
+++ Iteration-3/predictor.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, render_template_string
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
 @app.route('/', methods=['GET'])
-
-def hello_world():
-    return render_template('index.html')
+def predict():
+    feeling = request.args.get('feelings')
+    
+    return render_template('output.html', prediction = feeling)
+    
 
 
 if __name__ == '__main__':
