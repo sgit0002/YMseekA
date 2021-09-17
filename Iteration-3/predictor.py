fa2fb062241cf9874c6437e6c6d1713f88dbb0ba
Ram <sgit0002@student.monash.edu>
2021-09-17 21:00:58 +1000

    adding lstm model

--- Iteration-3/predictor.py
+++ Iteration-3/predictor.py
@@ -1,4 +1,18 @@
 from flask import Flask, render_template, request, render_template_string
+import tensorflow as tf
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+import numpy as np
+
+
+max_words = 5000
+max_len = 200
+
+model = tf.keras.models.load_model('model')
+
+sentiment = ["Not Depressed", "Depressed"]
+tokenizer = Tokenizer(num_words=max_words)
+
 
 app = Flask(__name__, template_folder="templates")
 
@@ -10,7 +24,12 @@ def index():
 def predict():
     feeling = request.args.get('feelings')
     
-    return render_template('output.html', prediction = feeling)
+    sequence = tokenizer.texts_to_sequences([feeling])
+    test = pad_sequences(sequence, maxlen=max_len)
+    
+    pred = sentiment[np.around(model.predict(test), decimals=0).argmax(axis=1)[0]]
+
+    return render_template('output.html', prediction = pred)
     
 
 
