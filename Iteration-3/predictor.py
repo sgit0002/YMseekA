ec24e51696fa3123e563eeba323cd526aac721ca
Ram <sgit0002@student.monash.edu>
2021-09-21 20:46:47 +1000

    adding latest model and webpage

--- Iteration-3/predictor.py
+++ Iteration-3/predictor.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, request, render_template_string
+
 import tensorflow as tf
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 import numpy as np
 
+max_words = 112544
+max_len = 500
 
-max_words = 5000
-max_len = 200
-
-
+model = tf.keras.models.load_model('model-bi-new.h5')
 
 
 app = Flask(__name__, template_folder="templates")
@@ -17,21 +17,20 @@ app = Flask(__name__, template_folder="templates")
 def index():
     return render_template('index.html')
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def predict():
     feeling = request.args.get('feelings')
-    feeling = str(str(feeling).split('+'))
-
-    model = tf.keras.models.load_model('model-bi')
+    # feeling = str(str(feeling).split('+'))
 
     sentiment = ["Not Depressed", "Depressed"]
     tokenizer = Tokenizer(num_words=max_words)
     sequence = tokenizer.texts_to_sequences([feeling])
+    print(sequence)
     test = pad_sequences(sequence, maxlen=max_len)
     
     pred = sentiment[np.around(model.predict(test), decimals=0).argmax(axis=1)[0]]
-
-    return render_template('output.html', prediction = pred)
+    print(pred)
+    return render_template('output.html', prediction = feeling)
     
 
 
