67428d10b5ed0aece0e0afb936ae7c4fa95c0b77
Ram <sgit0002@student.monash.edu>
2021-09-22 00:43:43 +1000

    model deploy

--- Iteration-3/predictor.py
+++ Iteration-3/predictor.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template, request, render_template_string
 
-import tensorflow as tf
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-import numpy as np
 
-max_words = 112544
+from keras.preprocessing.text import tokenizer_from_json
+from keras.preprocessing.sequence import pad_sequences
+import numpy as np, json
+from keras.models import load_model
+
+max_words = 5000
 max_len = 500
 
-model = tf.keras.models.load_model('model-bi-new.h5')
+with open('tokenizer.json') as f:
+    data = json.load(f)
+    tokenizer = tokenizer_from_json(data)
 
+model = load_model('model-bi-new.h5')
+
+def preprocess():
+    return 
 
 app = Flask(__name__, template_folder="templates")
 
@@ -20,17 +27,21 @@ def index():
 @app.route('/', methods=['GET', 'POST'])
 def predict():
     feeling = request.args.get('feelings')
-    # feeling = str(str(feeling).split('+'))
-
+    feeling = str(feeling)
+    
     sentiment = ["Not Depressed", "Depressed"]
-    tokenizer = Tokenizer(num_words=max_words)
+    
+    tokenizer.fit_on_texts([feeling])
     sequence = tokenizer.texts_to_sequences([feeling])
-    print(sequence)
+    
     test = pad_sequences(sequence, maxlen=max_len)
     
     pred = sentiment[np.around(model.predict(test), decimals=0).argmax(axis=1)[0]]
-    print(pred)
-    return render_template('output.html', prediction = feeling)
+    index = model.predict(test).argmax(axis=1)[0]
+    perc = model.predict(test)[0][index] * 100
+    perc = np.round(perc, decimals=2)
+
+    return render_template('output.html', prediction = pred, perc = str(perc)+'%')
     
 
 
